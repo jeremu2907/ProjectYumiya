c5640f5a535bc68bdf6c06015ddcbd1722562caf
jeremu2907 <jeremu2907@gmail.com>
2022-10-26 14:03:34 -0500

    'something'

--- yumiya/push.py
+++ yumiya/push.py
@@ -3,12 +3,14 @@ import sys
 
 os.system("git add .")
 try:
-    s = ""
+    s = "'"
     for i in sys.argv:
         s += i + ' '
+    s = "'"
     os.system("git commit -m '" + sys.argv[1] + "'")
 except:
     print("No argument, using standard commit message")
     os.system("git commit -m 'Commit new changes'")
 finally:
     os.system("git push")
+
