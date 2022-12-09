b9bb0f9183f3b19dee8bdd261216f66729e16578
jeremu2907 <jeremu2907@gmail.com>
2022-12-09 13:54:01 -0600

    sort event by time and date

--- yumiya/src/Component/DailyCard/updateEvent.js
+++ yumiya/src/Component/DailyCard/updateEvent.js
@@ -1,16 +1,26 @@
 // This function invoked seconds once the calendar is loaded
+function compare( a, b ) {
+    if ( a.date < b.date ){
+      return -1;
+    }
+    else if ( a.date > b.date ){
+      return 1;
+    }
+    else {
+        if ( a.dateTime < b.dateTime ){
+            return -1;
+        }
+        else if ( a.dateTime > b.dateTime ){
+            return 1;
+        }
+        else return 0;
+    }
+}
 
 export async function listUpcomingEvents() {
     /* global gapi */
     let response;
     let eventList = []
-
-    // fetch("https://www.googleapis.com/calendar/v3/users/me/calendarList")
-    // .then((response) => {
-    //     return response.json();
-    // }).then(data => {
-    //     console.log(data)
-    // })
 
     let calendarList = await gapi.client.calendar.calendarList.list();
     // console.log(calendarList.result.items)
@@ -53,45 +63,6 @@ export async function listUpcomingEvents() {
         }
     }
 
-
-    // try {
-    //     const request = {
-    //         'calendarId': 'primary',
-    //         'timeMin': (new Date()).toISOString(),
-    //         'showDeleted': false,
-    //         'singleEvents': true,
-    //         'maxResults': 10,
-    //         'orderBy': 'startTime',
-    //     };
-        
-    //     response = await gapi.client.calendar.events.list(request);
-        
-    // } catch {
-    //     return 1;
-    // }
-
-    // const events = response.result.items;
-    // if (!events || events.length === 0) {
-    //     console.log( 'No events found.');
-    //     return;
-    // }
-    
-    
-    // for(let i = 0; i < events.length; i++){
-    //     const event = {
-    //         name : events[i].summary,
-    //         dateTime : events[i].start.dateTime,
-    //         date : events[i].start.date,
-    //         id : events[i].id,
-    //         location: events[i].location,
-    //         description: events[i].description
-    //     }
-
-    //     eventList.push(event)
-    // }
-    
-    //Pushing to localStorage
-    // window.localStorage.setItem("eventList",JSON.stringify(eventList))
+    eventList.sort( compare )
     return eventList
-
 }
